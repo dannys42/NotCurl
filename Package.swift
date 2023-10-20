94c6e1d9f362e4ef3126cbffab37b9d91849efb8
Danny Sung <danny@dannysung.com>
2023-10-19 17:25:09 -0700

    SPM: add executable product

--- Package.swift
+++ Package.swift
@@ -6,6 +6,10 @@ import PackageDescription
 let package = Package(
     name: "NotCurl",
     platforms: [ .macOS(.v13) ],
+    products: [
+        .executable(name: "not-curl",
+                    targets: [ "not-curl" ])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
